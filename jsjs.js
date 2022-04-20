d286f88224f8006e80952796c1d520364c608578
hikiyer <63396872+hikiyer@users.noreply.github.com>
2022-04-20 20:26:06 +0300

    Update jsjs.js

--- jsjs.js
+++ jsjs.js
@@ -8,12 +8,25 @@ $('.icon-edit').one('click', function() {
                   var idParent = valParent.replace(/[^0-9]/g,"");
                   if ( idParentTagCookie !== idParent+idTag){
                         $.cookie('idTagParent', idParent+idTag);
-                        // console.log(idTag);
-                        // console.log(idTag);
-                        // console.log('idTagCookie-' + idParentCookie);
                         var textTag = $('#noteinput_'+idTag +'_note').val();
-                        console.log(textTag);
                         widget_dashboard('add', null , 'tag', textTag, false, dropable = true, idParent);
+                        var idReload = '#edit_pop_over_oncard_'+idParent;
+                        $(idReload).append( '<div id="preloader_partial"  class="preloader_trello"></div>');
+                  }
+            }else if($(this).hasClass('fa-times')){
+                  var idParentTagCookie;
+                  idParentTagCookie = $.cookie('idTagParent')
+                  var id_button = $(this).attr('id');
+                  var valParent =  $(this).attr('data-value');
+                  var idTag = id_button.replace(/[^0-9]/g,"");
+                  var idParent = valParent.replace(/[^0-9]/g,"");
+                  if ( idParentTagCookie !== idParent+idTag){
+                        $.cookie('idTagParent', idParent+idTag);
+                        var idReload = '#edit_pop_over_oncard_'+idParent;
+                        $(idReload).append( '<div id="preloader_partial"  class="preloader_trello"></div>');
+                  widget_dashboard('delete', idTag, null );
+                  $('#card_element_'+idTag).remove();
+                        return false;
                   }
             }
       });
